fd26a31db950694d3aca9137794188742dc497e6
Joe Pritchett <joempritchett@gmail.com>
2019-03-22 16:42:52 -0700

    add html

--- src/main.js
+++ src/main.js
@@ -3,3 +3,10 @@ import 'bootstrap/scss/bootstrap.scss';
 import 'jquery';
 import 'jquery/src/jquery.js';
 import './sass/styles.scss';
+
+$(document).ready(function(){
+  $('.submit').click(function(){
+    const input = $('.search').val();
+    console.log(input);
+  });
+});
