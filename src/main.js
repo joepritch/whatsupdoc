a9c53262fb4c39c9748bcfb91a28e41a653724eb
Joe Pritchett <joempritchett@gmail.com>
2019-03-28 01:05:51 -0700

    add doctor website to search

--- src/main.js
+++ src/main.js
@@ -12,12 +12,12 @@ $(document).ready(function(){
     const query = $('.search').val();
     const instance = new DocSearch();
     const promise = instance.getList(query);
-
     promise.then(function(response) {
       const body = JSON.parse(response);
       if (body.data.length === 0) {
         $('.result').text(`No results for "${query}"`)
       } else {
+        console.log(body.data[0].practices);
         $('.name').text(`${body.data[0].practices[0].name}`);
         $('.patients').text(`${body.data[0].practices[0].accepts_new_patients}`);
         $('.state').text(`${body.data[0].practices[0].visit_address.state}`);
@@ -25,6 +25,7 @@ $(document).ready(function(){
         $('.street').text(`${body.data[0].practices[0].visit_address.street}`);
         $('.zip').text(`${body.data[0].practices[0].visit_address.zip}`);
         $('.phone').text(`${body.data[0].practices[0].phones[0].number}`);
+        $('.website').text(`${body.data[0].practices[0].website}`)
       }
       }, function(error) {
         $('.result').text(`There was an error processing your request: ${error.message}`);
