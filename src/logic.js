31778075da34e86bcd81521ee03e752ed1b47017
Joe Pritchett <joempritchett@gmail.com>
2019-03-27 23:55:55 -0700

    refactor and separate logic

---
+++ src/logic.js
@@ -0,0 +1,18 @@
+export class DocSearch {
+  getList(query) {
+    return new Promise(function(resolve, reject) {
+      const key = process.env.exports.apiKey;
+      const request = new XMLHttpRequest();
+      const url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${query}&location=45.5202%2C-122.6742%2C10&fields=practices(name%2Caccepts_new_patients%2Cphones%2Cvisit_address(city%2Cstate%2Cstreet%2Czip))&skip=0&limit=10&user_key=${key}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
--- src/main.js
+++ src/main.js
@@ -3,43 +3,31 @@ import 'bootstrap/scss/bootstrap.scss';
 import 'jquery';
 import 'jquery/src/jquery.js';
 import './sass/styles.scss';
-// import { DocSearch } from './logic.js'
+import { DocSearch } from './logic.js'
 
 $(document).ready(function(){
   const key = process.env.exports.apiKey;
 
   $('.submit').click(function(){
     const query = $('.search').val();
-    console.log(query);
+    const instance = new DocSearch();
+    const promise = instance.getList(query);
 
-    let request = new XMLHttpRequest();
-    const url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${query}&location=45.5202%2C-122.6742%2C10&fields=practices(name%2Caccepts_new_patients%2Cphones%2Cvisit_address(city%2Cstate%2Cstreet%2Czip))&skip=0&limit=10&user_key=${key}`;
-    console.log(url);
-
-    request.onreadystatechange = function() {
-      if (this.readyState === 4 && this.status === 200) {
-        const response = JSON.parse(this.responseText);
-        console.log(response);
-        console.log(response.data[0].practices[0].name);
-        console.log(response.data[0].practices[0].accepts_new_patients);
-        console.log(response.data[0].practices[0].visit_address.city);
-        console.log(response.data[0].practices[0].visit_address.state);
-        console.log(response.data[0].practices[0].visit_address.street);
-        console.log(response.data[0].practices[0].visit_address.zip);
-        console.log(response.data[0].practices[0].phones[0].type);
-        console.log(response.data[0].practices[0].phones[0].number);
-        getElements(response);
+    promise.then(function(response) {
+      const body = JSON.parse(response);
+      if (body.data.length === 0) {
+        $('.result').text(`No results for "${query}"`)
+      } else {
+        $('.name').text(`${body.data[0].practices[0].name}`);
+        $('.patients').text(`${body.data[0].practices[0].accepts_new_patients}`);
+        $('.state').text(`${body.data[0].practices[0].visit_address.state}`);
+        $('.city').text(`${body.data[0].practices[0].visit_address.city}`);
+        $('.street').text(`${body.data[0].practices[0].visit_address.street}`);
+        $('.zip').text(`${body.data[0].practices[0].visit_address.zip}`);
+        $('.phone').text(`${body.data[0].practices[0].phones[0].number}`);
       }
-    }
-
-    request.open("GET", url, true);
-    request.send();
-
-   const getElements = function(response) {
-      const iteration = 0;
-      response.data[].forEach()
-      $('.name').text(`${response.data[0].practices[0].name}`);
-      $('.patients').text(`${response.data[0].practices[0].accepts_new_patients}`);
-    }
+      }, function(error) {
+        $('.result').text(`There was an error processing your request: ${error.message}`);
+    });
   });
 });
