c130e964a2bd5254346435a4614825b9adb8c48b
Joe Pritchett <joempritchett@gmail.com>
2019-03-31 18:30:33 -0700

    refactor and dry up code

--- src/logic.js
+++ src/logic.js
@@ -1,9 +1,7 @@
 export class DocSearch {
-  getList(query) {
+  getList(url) {
     return new Promise(function(resolve, reject) {
-      const key = process.env.exports.apiKey;
       const request = new XMLHttpRequest();
-      const url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${query}&location=45.5202%2C-122.6742%2C10&fields=practices(name%2Caccepts_new_patients%2Cphones%2Cvisit_address(city%2Cstate%2Cstreet%2Czip)website)&skip=0&limit=10&user_key=${key}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
--- src/main.js
+++ src/main.js
@@ -7,28 +7,48 @@ import { DocSearch } from './logic.js'
 
 $(document).ready(function(){
   const key = process.env.exports.apiKey;
-
-  $('.submit').click(function(){
-    const query = $('.search').val();
+  $('.nameSearch').click(function(){
+    const name = $('.name').val();
+    const nameSearchURL = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=45.5202%2C-122.6742%2C10&fields=practices(name%2Caccepts_new_patients%2Cphones%2Cvisit_address(city%2Cstate%2Cstreet%2Czip)website)&skip=0&limit=10&user_key=${key}`;
+    displayResults(nameSearchURL, name);
+  });
+  $('.keywordSearch').click(function(){
+    const keyword = $('.keyword').val();
+    const keywordSearchURL = `https://api.betterdoctor.com/2016-03-01/doctors?query=${keyword}&location=45.5202%2C-122.6742%2C10&fields=practices(name%2Caccepts_new_patients%2Cphones%2Cvisit_address(city%2Cstate%2Cstreet%2Czip)website)&skip=0&limit=10&user_key=${key}`;
+    displayResults(keywordSearchURL, keyword);
+  });
+  function displayResults(url, query){
     const instance = new DocSearch();
-    const promise = instance.getList(query);
+    const promise = instance.getList(url);
     promise.then(function(response) {
+      $('.result').text("");
       const body = JSON.parse(response);
       if (body.data.length === 0) {
         $('.result').text(`No results for "${query}"`)
       } else {
-        console.log(body.data[0].practices);
-        $('.name').text(`${body.data[0].practices[0].name}`);
-        $('.patients').text(`${body.data[0].practices[0].accepts_new_patients}`);
-        $('.state').text(`${body.data[0].practices[0].visit_address.state}`);
-        $('.city').text(`${body.data[0].practices[0].visit_address.city}`);
-        $('.street').text(`${body.data[0].practices[0].visit_address.street}`);
-        $('.zip').text(`${body.data[0].practices[0].visit_address.zip}`);
-        $('.phone').text(`${body.data[0].practices[0].phones[0].number}`);
-        $('.website').text(`${body.data[0].practices[0].website}`)
+        body.data.forEach(list);
+        function list(input){
+          $('.result').append(`${input.practices[0].name}` + '<br>');
+          if (input.practices[0].accepts_new_patients == true) {
+            $('.result').append('Currently accepting patients' + '<br>');
+          } else {
+            $('.result').append('Not currently accepting patients' + '<br>');
+          }
+          $('.result').append(`${input.practices[0].visit_address.state}` + '<br>');
+          $('.result').append(`${input.practices[0].visit_address.city}` + '<br>');
+          $('.result').append(`${input.practices[0].visit_address.street}` + '<br>');
+          $('.result').append(`${input.practices[0].visit_address.zip}` + '<br>');
+          $('.result').append(`${input.practices[0].phones[0].number}` + '<br>');
+          if (input.practices[0].website == undefined) {
+            $('.result').append('No website found' + '<br>');
+          } else {
+            $('.result').append(`${input.practices[0].website}` + '<br>');
+          }
+          $('.result').append('<hr>');
+        }
       }
-      }, function(error) {
-        $('.result').text(`There was an error processing your request: ${error.message}`);
+    }, function(error) {
+      $('.result').text(`There was an error processing your request: ${error.message}`);
     });
-  });
+  }
 });
